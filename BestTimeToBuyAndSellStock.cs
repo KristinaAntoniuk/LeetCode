ba5045119f45a9eb859b20155b85e07b91117d2f
Kristina Antoniuk <kristina_antoniuk@outlook.com>
2023-11-26 13:22:17 +0100

    Interview 150 +2 (8)

---
+++ BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,65 @@
+﻿namespace LeetCode
+{
+    public class BestTimeToBuyAndSellStock
+    {
+        public int MaxProfit(int[] prices)
+        {
+            if (prices.Length == 0) return 0;
+
+            int min = prices[0];
+            int maxProfit = 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] < min)
+                {
+                    min = prices[i];
+                }
+                else
+                {
+                    int profit = prices[i] - min;
+                    if (profit > maxProfit)
+                    {
+                        maxProfit = profit;
+                    }
+                }
+            }
+
+            return maxProfit;
+        }
+
+        public int MaxProfitII(int[] prices)
+        {
+            if (prices.Length == 0) return 0;
+
+            int min = prices[0];
+            int profit = 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] < min)
+                {
+                    min = prices[i];
+                }
+                else
+                {
+                    if (i == prices.Length - 1) 
+                    {
+                        if (prices[i] > min)
+                        {
+                            profit += prices[i] - min;
+                        }
+                    }
+                    else if (prices[i + 1] < prices[i])
+                    {
+                        profit += prices[i] - min;
+                        min = prices[i + 1];
+                        i++;
+                    }
+                }
+            }
+
+            return profit;
+        }
+    }
+}
---
+++ LeetCodeTests/BestTimeToBuyAndSellStockTests.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCodeTests
+{
+    [TestClass]
+    public class BestTimeToBuyAndSellStockTests
+    {
+        BestTimeToBuyAndSellStock bestTimeToBuyAndSellStock;
+
+        public BestTimeToBuyAndSellStockTests()
+        {
+            bestTimeToBuyAndSellStock = new BestTimeToBuyAndSellStock();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] input = [7, 1, 5, 3, 8, 4, 2, 10];
+            int result = 17;
+            Assert.AreEqual(result, bestTimeToBuyAndSellStock.MaxProfitII(input));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] input = [3, 3];
+            int result = 0;
+            Assert.AreEqual(result, bestTimeToBuyAndSellStock.MaxProfitII(input));
+        }
+    }
+}
