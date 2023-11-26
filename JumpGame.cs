a1d7c8814b96d53ae6e46f7abe3096e8891d3c8a
Kristina Antoniuk <kristina_antoniuk@outlook.com>
2023-11-26 17:08:13 +0100

    Interview 150 +2 (10)

---
+++ JumpGame.cs
@@ -0,0 +1,57 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace LeetCode
+{
+    public class JumpGame
+    {
+        public bool CanJump(int[] nums)
+        {
+            if (nums.Length == 0) return false;
+
+            int lastIndex = nums.Length - 1;
+
+            for (int i = lastIndex; i >= 0; i--)
+            {
+                if (i + nums[i] >= lastIndex)
+                {
+                    if (i == 0) return true;
+
+                    lastIndex = i;
+                }
+            }
+
+            return false;
+        }
+
+        public int CanJumpII(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+
+            int lastIndex = nums.Length - 1;
+            int jumps = 0;
+
+
+            for (int i = lastIndex; i >= 0; i--)
+            {
+                int? max = null;
+
+                for (int j = i; j >= 0; j--)
+                {
+                    if (j + nums[j] >= lastIndex)
+                    {
+                        max = j;
+                    }
+                }
+
+                if (max != null)
+                {
+                    lastIndex = max.Value;
+                    i = lastIndex;
+                    jumps++;
+                }
+            }
+
+            return jumps;
+        }
+    }
+}
---
+++ LeetCodeTests/JumpGameTests.cs
@@ -0,0 +1,63 @@
+﻿namespace LeetCodeTests
+{
+    [TestClass]
+    public class JumpGameTests
+    {
+        JumpGame jumpGame;
+
+        public JumpGameTests()
+        {
+            jumpGame = new JumpGame();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] array = [2, 3, 1, 1, 4];
+            Assert.IsTrue(jumpGame.CanJump(array));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] array = [3, 2, 1, 0, 4];
+            Assert.IsFalse(jumpGame.CanJump(array));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int[] array = [2, 0];
+            Assert.IsTrue(jumpGame.CanJump(array));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int[] array = [1, 1, 1, 0];
+            Assert.IsTrue(jumpGame.CanJump(array));
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            int[] array = [1, 1, 2, 2, 0, 1, 1];
+            Assert.IsTrue(jumpGame.CanJump(array));
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            int[] array = [5, 9, 3, 2, 1, 0, 2, 3, 3, 1, 0, 0];
+            Assert.IsTrue(jumpGame.CanJump(array));
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            int[] array = [2, 3, 1, 1, 4];
+            Assert.AreEqual(2, jumpGame.CanJumpII(array));
+        }
+
+    }
+}
