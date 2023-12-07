95d29417ffc8fccdccbf00ad1c800533da66d93a
Kristina Antoniuk <kristina_antoniuk@outlook.com>
2023-12-07 13:21:52 +0100

    Interview 150 +1 (25)

---
+++ LeetCodeTests/ValidPalindromeTests.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCodeTests
+{
+    [TestClass]
+    public class ValidPalindromeTests
+    {
+        ValidPalindrome isPalindrome;
+
+        public ValidPalindromeTests()
+        {
+            isPalindrome = new ValidPalindrome();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string input = "A man, a plan, a canal: Panama";
+            Assert.AreEqual(true, isPalindrome.IsPalindrome(input));
+        }
+    }
+}
---
+++ ValidPalindrome.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCode
+{
+    public class ValidPalindrome
+    {
+        public bool IsPalindrome(string s)
+        {
+            s = s.ToLower().Trim();
+            int rightIndex = s.Length - 1;
+            int leftIndex = 0; 
+
+            while (leftIndex < s.Length && rightIndex >= 0)
+            {
+                if (!Char.IsLetter(s[leftIndex]))
+                {
+                    if (!Char.IsDigit(s[leftIndex]))
+                    {
+                        leftIndex++;
+                        continue;
+
+                    }
+                }
+
+                if (!Char.IsLetter(s[rightIndex]))
+                {
+                    if (!Char.IsDigit(s[rightIndex]))
+                    {
+                        rightIndex--;
+                        continue;
+                    }
+                }
+
+                if (s[leftIndex] != s[rightIndex])
+                {
+                    return false;
+                }
+                else
+                {
+                    leftIndex++;
+                    rightIndex--;
+                }
+
+            }
+
+            return true;
+        }
+    }
+}
